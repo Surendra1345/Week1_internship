996f21e8a4d2972fb69a5799b3ab93866b5584ce
surendrareddy <skamired@gitam.in>
2026-05-31 16:59:02 +0530

    Day 2-Variables done

---
+++ day2/variables.py
@@ -0,0 +1,11 @@
+name="Surendra"
+age=21
+city="chennai"
+is_intern=True
+print(name)
+print(age)
+print(city)
+print(is_intern)
+print("My name is"+name)
+print("I am"+str(age)+"years old")
+print("I am from"+city)
