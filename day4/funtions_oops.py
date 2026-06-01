60bc75a0916f2191adb2db5a3c921ad3b9892ccc
surendrareddy <skamired@gitam.in>
2026-06-01 11:52:56 +0530

    Day 4 functions

---
+++ day4/funtions_oops.py
@@ -0,0 +1,56 @@
+# functions
+
+def greet(name):
+    print("Hello,"+name)
+def add(a,b):
+    return a+b
+greet("surendra")
+print(add(5,10))
+
+#ARGS 
+def calculate(*args):
+    total=0
+    for item in args:
+        total+=item
+    return total
+    print(calculate(2,39))
+    print(calculate(5,10,15))
+#KWARGS
+def show_info(**kwargs):
+    for key,value in kwargs.items():
+        print(key,"=",value)
+    show_info(name="dhoni",age=39)
+    print()
+    show_info(name="dhoni",age=39,team="CSK",natioan="India")
+
+#OOPS
+class phone:
+    def __init__(self, brand, model, charge):
+        self.brand= brand
+        self.model= model
+        self.charge= charge
+    def call(self):
+        print(self.brand+" calling...")
+    def message(self):
+        print(self.brand+" messaging")
+   
+
+class smartphone(phone):
+    def __init__(self, brand, model, charge, app_store):
+        super().__init__(brand, model, charge)
+        self.app_store=app_store
+    def install_app(self,app_name):
+        print("installing",app_name)
+    def take_photo(self):
+        print("taking photo")
+phone1=phone("sumsung","glaxy",100)
+phone2=phone("Google","pixel",80)
+phone1.call()
+phone1.message()
+smartphone1=smartphone("Realme","11pro",90,"playstore")
+smartphone1.call()
+smartphone1.message()
+smartphone1.install_app("WhatsApp")
+smartphone1.take_photo()    
+
+    
